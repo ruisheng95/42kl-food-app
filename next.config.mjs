8e7d07bcfeff8624fa30d3d0063a454f9f4fa975
Ruisheng Ng <16008988+ruisheng95@users.noreply.github.com>
2024-09-23 21:55:12 +0800

    setup for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  basePath: "/42kl-food-app",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
